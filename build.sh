dae2877e219eddb55e2b5e94afc18c0191dd7e54
Re4son Protagonist <Re4son@users.noreply.github.com>
2016-05-22 09:24:07 +1000

    Update build.sh

--- build.sh
+++ build.sh
@@ -191,7 +191,10 @@ cp .config $KERNEL_BUILDER_DIR/v1_saved_config
 echo "**** COMPILING v1 KERNEL ****"
 ARCH=arm CROSS_COMPILE=${CCPREFIX} make -j${NUM_CPUS} -k zImage modules dtbs
 ARCH=arm CROSS_COMPILE=${CCPREFIX} INSTALL_MOD_PATH=${MOD_DIR} make -j${NUM_CPUS} modules_install
-${TOOLS_DIR}/mkimage/mkknlimg arch/arm/boot/zImage $PKG_DIR/boot/kernel.img
+## mkknlimg is no longer in tools
+## ${TOOLS_DIR}/mkimage/mkknlimg arch/arm/boot/zImage $PKG_DIR/boot/kernel.img
+## It is now found in the scripts directory of the Linux tree, where they are covered by the kernel licence
+${V1_DIR}/scripts/mkknlimg arch/arm/boot/zImage $PKG_DIR/boot/kernel.img
 cp -r ${MOD_DIR}/lib/* ${PKG_DIR}
 
 # RasPi v2 build
@@ -217,7 +220,10 @@ ARCH=arm CROSS_COMPILE=${CCPREFIX} INSTALL_MOD_PATH=${MOD_DIR} make -j${NUM_CPUS
 cp arch/arm/boot/dts/*.dtb $PKG_DIR/boot/
 cp arch/arm/boot/dts/overlays/*.dtb* $PKG_DIR/boot/overlays/
 cp arch/arm/boot/dts/overlays/README $PKG_DIR/boot/overlays/
-${TOOLS_DIR}/mkimage/mkknlimg arch/arm/boot/zImage $PKG_DIR/boot/kernel7.img
+## mkknlimg is no longer in tools
+## ${TOOLS_DIR}/mkimage/mkknlimg arch/arm/boot/zImage $PKG_DIR/boot/kernel7.img
+## It is now found in the scripts directory of the Linux tree, where they are covered by the kernel licence
+${V1_DIR}/scripts/mkknlimg arch/arm/boot/zImage $PKG_DIR/boot/kernel7.img
 cp -r ${MOD_DIR}/lib/* ${PKG_DIR}
 
 # copy overlays
