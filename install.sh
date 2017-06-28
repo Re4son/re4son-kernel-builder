d431743656a55917a3da757fa42244bb0c4e896d
Re4son Protagonist <Re4son@users.noreply.github.com>
2017-06-28 19:15:06 +1000

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
 
-if [[ $EUID -ne 0 ]]; then
-   echo "install.sh must be run as root. try: sudo install.sh"
-   exit 1
-fi
+PROG_NAME="$(basename $0)"
+ARGS="$@"
+VERSION="4.4-1.1.11"
+
+function print_version() {
+    printf "\tRe4son-Kernel Installer: $PROG_NAME $VERSION\n\n"
+    exit 0
+}
+
+function print_help() {
+    printf "\n\tUsage: ${PROG_NAME} [option]\n"
+    printf "\t\t\t   (No option)\tInstall Re4son-Kernel and ask to install Re4son Bluetooth support\n"
+    printf "\t\t\t\t-h\tPrint this help\n"
+    printf "\t\t\t\t-e\tOnly install Re4son-Kernel headers\n"
+    printf "\t\t\t\t-b\tOnly install Re4son Bluetooth support for RPi3 & RPi Zero W\n"
+    printf "\t\t\t\t-r\tOnly remove Re4son Bluetooth support\n"
+    printf "\t\t\t\t-u\tUpdate Re4son-Kernel Installer\n\n"
+    exit 1
+}
 
 function ask() {
     # http://djm.me/ask
@@ -21,6 +36,7 @@ function ask() {
         fi
 
         # Ask the question
+        printf "\t++++ "
         read -p "$1 [$prompt] " REPLY
 
         # Default?
@@ -36,31 +52,41 @@ function ask() {
     done
 }
 
-# via: http://stackoverflow.com/a/5196108
 function exitonerr {
+    # via: http://stackoverflow.com/a/5196108
+    "$@"
+    local status=$?
 
-  "$@"
-  local status=$?
+    if [ $status -ne 0 ]; then
+        echo "Error completing: $1" >&2
+        exit 1
+    fi
 
-  if [ $status -ne 0 ]; then
-    echo "Error completing: $1" >&2
-    exit 1
-  fi
+    return $status
+}
 
-  return $status
-
+function check_update() {
+    TEMP_FILE="/tmp/install.sh"
+    printf "\n\t*** Downloading update ***\n"
+    wget -O ${TEMP_FILE} https://github.com/Re4son/re4son-kernel-builder/raw/build-4.4.y/install.sh
+    cp $TEMP_FILE $PROG_NAME
+    chmod +x $PROG_NAME
+    rm -f $TEMP_FILE
+    printf "\tReplaced old version:\tRe4son-Kernel Installer: ${PROG_NAME} ${VERSION}\n"
+    printf "\tWith new version:"
+    source "$PROG_NAME" -v
+    exit 0
 }
 
 function install_bluetooth {
-    echo "**** Installing bluetooth packages for Raspberry Pi 3 & Zero W ****"
+    printf "\n\t**** Installing bluetooth packages for Raspberry Pi 3 & Zero W ****\n"
     ARCH=`dpkg --print-architecture`
-    apt install bluez-firmware
 
     ## Install dependencies
-    PKG_STATUS=$(dpkg-query -W --showformat='${Status}\n' libreadline6|grep "install ok installed")
-    echo "Checking for libreadline6:" $PKG_STATUS
+    PKG_STATUS=$(dpkg-query -W --showformat='${Status}\n' libreadline6|grep "ok installed")
+    printf "\t**** Checking for libreadline6: ${PKG_STATUS} ****\n"
     if [ "" == "$PKG_STATUS" ]; then
-        echo "Fixing unmet dependencies. Installing libreadline6."
+        printf "Fixing unmet dependencies. Installing libreadline6.\n"
         if [ "armel" == "$ARCH" ]; then
             dpkg -i ./repo/libreadline6_6.3-8+b3_armel.deb
         else
@@ -68,13 +94,37 @@ function install_bluetooth {
         fi
     fi
 
+    ## Check for existing installations
+    PKG_STATUS=$(dpkg-query -W --showformat='${Status}\n' bluez|grep "ok installed")
+    printf "\t**** Checking for existing bluez installation: ${PKG_STATUS} ****\n"
+    if [ "" != "$PKG_STATUS" ]; then
+        PKG_SOURCE=$(dpkg-query -W bluez|grep "re4son")
+        if [ "" == "$PKG_SOURCE" ]; then
+            printf "\t**** Found incompatible bluez package. Removing it... ****\n"
+            apt remove -y bluez
+            printf "\t**** Incompatible bluez package removed. ****\n"
+        fi
+
+    fi
+    PKG_STATUS=$(dpkg-query -W --showformat='${Status}\n' pi-bluetooth|grep "ok installed")
+    printf "\t**** Checking for existing pi-bluetooth installation: ${PKG_STATUS} ****\n"
+    if [ "" != "$PKG_STATUS" ]; then
+        PKG_SOURCE=$(dpkg-query -W pi-bluetooth|grep "re4son")
+        if [ "" == "$PKG_SOURCE" ]; then
+            printf "\t**** Found incompatible pi-bluetooth package. Removing it... ****\n"
+            apt remove -y pi-bluetooth
+            printf "\t**** Incompatible pi-bluetooth package removed. ****\n"
+        fi
+    fi
+
+    apt install bluez-firmware
 
     if [ "armel" == "$ARCH" ]; then
-        dpkg -i ./repo/bluez_5.39-1+rpi1+re4son_armel.deb
+        dpkg --force-all -i ./repo/bluez_5.39-1+rpi1+re4son_armel.deb
     else
-        dpkg -i ./repo/bluez_5.39-1+rpi3+re4son_armhf.deb
+        dpkg --force-all -i ./repo/bluez_5.39-1+rpi3+re4son_armhf.deb
     fi
-    dpkg -i ./repo/pi-bluetooth_0.1.4+re4son_all.deb
+    dpkg --force-all -i ./repo/pi-bluetooth_0.1.4+re4son_all.deb
     apt-mark hold bluez-firmware bluez pi-bluetooth
 
     if [ ! -f  /lib/udev/rules.d/50-bluetooth-hci-auto-poweron.rules ]; then
@@ -93,15 +143,18 @@ function install_bluetooth {
             sed -i 's/^RestrictAddressFamilies=AF_UNIX AF_NETLINK AF_INET AF_INET6.*/RestrictAddressFamilies=AF_UNIX AF_NETLINK AF_INET AF_INET6 AF_BLUETOOTH/' /etc/systemd/system/systemd-udevd.service
         fi
     fi
-    if ask "Enable bluetooth services?"; then
+    printf "\t**** Bluetooth packages for Raspberry Pi 3 & Zero W installed ****\n\n"
+    if ask "Enable Bluetooth services?"; then
         systemctl unmask bluetooth.service
         systemctl enable bluetooth
         systemctl enable hciuart
+        printf "\t**** Bluetooth services enabled\n\n"
     fi
-    echo "**** Bluetooth packages for Raspberry Pi 3 & Zero W installed ****"
+    return 0
 }
+
 function install_firmware {
-    echo "**** Installing firmware for RasPi bluetooth chip ****"
+    printf "\n\t**** Installing firmware for RasPi bluetooth chip ****\n"
     #Raspberry Pi 3 & Zero W
     if [ ! -f /lib/firmware/brcm/BCM43430A1.hcd ]; then
         cp firmware/BCM43430A1.hcd /lib/firmware/brcm/BCM43430A1.hcd
@@ -117,54 +170,156 @@ function install_firmware {
     if [ ! -f /lib/firmware/brcm/brcmfmac43430-sdio.txt ]; then
         cp firmware/brcmfmac43430-sdio.txt /lib/firmware/brcm/brcmfmac43430-sdio.txt
     fi
-    echo
-    echo "**** Firmware installed ****"
-    return
+    printf "\t**** Firmware installed                           ****\n"
+    return 0
 }
 
-echo "**** Installing custom Re4son kernel with kali wifi injection patch and TFT support ****"
+function install_kernel(){
+    printf "\n\t**** Installing custom Re4son kernel with kali wifi injection patch and TFT support ****\n"
 
-## Install device-tree-compiler
-PKG_STATUS=$(dpkg-query -W --showformat='${Status}\n' device-tree-compiler|grep "install ok installed")
-echo "Checking for device-tree-compiler:" $PKG_STATUS
-if [ "" == "$PKG_STATUS" ]; then
-    echo "No device-tree-compiler. Installing it now."
-    apt update
-    apt install device-tree-compiler
+    ## Install device-tree-compiler
+    printf "\n\t**** Installing device tree overlays for various screens ****\n"
+    PKG_STATUS=$(dpkg-query -W --showformat='${Status}\n' device-tree-compiler|grep "ok installed")
+    printf "\t**** Checking for device-tree-compiler: ${PKG_STATUS} ****\n"
+    if [ "" == "$PKG_STATUS" ]; then
+        printf "\tNo device-tree-compiler. Installing it now.\n"
+        apt update
+        apt install device-tree-compiler
+    fi
+    ## Reserved
+    ## cp src dest
+    printf "\n\t**** Device tree overlays installed ****\n"
+    exitonerr dpkg --force-architecture -i --ignore-depends=raspberrypi-kernel raspberrypi-bootloader_*
+    exitonerr dpkg --force-architecture -i raspberrypi-kernel_*
+    exitonerr dpkg --force-architecture -i libraspberrypi0_*
+    exitonerr dpkg --force-architecture -i libraspberrypi-dev_*
+    exitonerr dpkg --force-architecture -i libraspberrypi-doc_*
+    exitonerr dpkg --force-architecture -i libraspberrypi-bin_*
+
+    printf "\n\t**** Fixing unmet dependencies in Kali Linux ****\n"
+    mkdir -p /etc/kbd
+    touch /etc/kbd/config
+    printf "\t**** Unmet dependencies in Kali Linux fixed ****\n"
+    printf "\t**** Installation completed ****\n"
+    printf "\t**** Documentation and help can be found in Sticky Finger's Kali-Pi forums at ****\n"
+    printf "\t**** https://whitedome.com.au/forums ****\n"
+    printf "\t**** next you can run the universal setup tool to activate your TFT screen via ****\n"
+    printf "\t**** ./re4son-pi-tft-setup -t [pitfttype] -d [home directory]****\n\n"
+    return 0
+}
+
+function remove_bluetooth {
+    printf "\n"
+    if ask "Remove Re4son-Kernel Bluetooth support?"; then
+        printf "\n\t**** Stopping bluetooth Services ****\n"
+
+        systemctl stop bluetooth
+        systemctl stop hciuart
+
+        printf "\t**** Removing bluetooth packages for Raspberry Pi 3 & Zero W ****\n"
+
+        ARCH=`dpkg --print-architecture`
+        apt-mark unhold bluez-firmware bluez pi-bluetooth
+
+        PKG_STATUS=$(dpkg-query -W pi-bluetooth|grep "re4son")
+        printf "\t**** Checking for Re4son\'s pi-bluetooth: ${PKG_STATUS} ****\n"
+        if [ "" != "$PKG_STATUS" ]; then
+            dpkg --force-all -P pi-bluetooth
+        fi
+
+        PKG_STATUS=$(dpkg-query -W bluez|grep "re4son")
+        printf "\t**** Checking for Re4son\'s bluez: ${PKG_STATUS}\n"
+        if [ "" != "$PKG_STATUS" ]; then
+            dpkg --force-all -P bluez
+        fi
+
+        dpkg --force-all -P libreadline6
+
+        rm -f /lib/udev/rules.d/50-bluetooth-hci-auto-poweron.rules
+        if [ -L  /bin/hciconfig ]; then
+            rm -f /bin/hciconfig
+        fi
+        if [ -f /lib/systemd/system/systemd-udevd.service ]; then
+            sed -i 's/AF_INET6 AF_BLUETOOTH/AF_INET6/' /lib/systemd/system/systemd-udevd.service
+        elif [ -f /etc/systemd/system/systemd-udevd.service ]; then
+            sed -i 's/AF_INET6 AF_BLUETOOTH/AF_INET6/' /etc/systemd/system/systemd-udevd.service
+        fi
+
+        printf "\t**** Bluetooth packages for Raspberry Pi 3 & Zero W removed ****\n\n"
+
+        if ask "Reboot to apply changes?"; then
+            reboot
+        fi
+    fi
+    printf "\n"
+    return 0
+}
+
+function install_headers() {
+
+    printf "\n**** Installing Re4son-Kernel headers ****\n"
+    exitonerr dpkg --force-architecture -i raspberrypi-kernel-headers_*
+    printf "\n**** Installation completed ****\n"
+    return 0
+}
+
+############
+##        ##
+##  MAIN  ##
+
+if [[ $EUID -ne 0 ]]; then
+   printf "\n\t${PROG_NAME} must be run as root. try: sudo install.sh\n\n"
+   exit 1
 fi
 
-exitonerr dpkg --force-architecture -i --ignore-depends=raspberrypi-kernel raspberrypi-bootloader_*
-exitonerr dpkg --force-architecture -i raspberrypi-kernel_*
-exitonerr dpkg --force-architecture -i libraspberrypi0_*
-exitonerr dpkg --force-architecture -i libraspberrypi-dev_*
-exitonerr dpkg --force-architecture -i libraspberrypi-doc_*
-exitonerr dpkg --force-architecture -i libraspberrypi-bin_*
+args=$(getopt -uo 'hevbru' -- $*)
 
-echo "**** Installing device tree overlays for various screens ****"
+set -- $args
 
-echo "**** Fixing unmet dependencies in Kali Linux ****"
-mkdir -p /etc/kbd
-touch /etc/kbd/config
-echo
-echo "**** Unmet dependencies in Kali Linux fixed ****"
-echo
+for i
+do
+    case "$i"
+    in
+        -h)
+            print_help
+            exit 0
+            ;;
+        -v)
+            print_version
+            exit 0
+            ;;
+        -e)
+            install_headers
+            exit 0
+            ;;
+        -b)
+            install_firmware
+            install_bluetooth
+            if ask "Reboot to apply changes?"; then
+                reboot
+            fi
+            exit 0
+            ;;
+        -r)
+            remove_bluetooth
+            exit 0
+            ;;
+        -u)
+            check_update
+            exit 0
+            ;;
+    esac
+done
 
-if ask "Install support for RasPi 3 & Zero W built-in wifi & bluetooth adapters?" "N"; then
-        install_firmware
-        install_bluetooth
+printf "\n"
+if ask "Install Re4son-Kernel?" "Y"; then
+    install_kernel
 fi
-
-
-
-echo "**** Documentation and help can be found in Sticky Finger's Kali-Pi forums at ****"
-echo "**** https://whitedome.com.au/forums ****"
-echo
-echo "**** next you can run the universal setup tool to activate your TFT screen via ****"
-echo "**** ./re4son-pi-tft-setup -t [pitfttype] -d [home directory]****"
-echo
-read -p "Reboot to apply changes? (y/n): " -n 1 -r
-echo
-
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  reboot
+if ask "Install support for RasPi 3 & Zero W built-in wifi & bluetooth adapters?"; then
+    install_firmware
+    install_bluetooth
 fi
+if ask "Reboot to apply changes?"; then
+    reboot
+fi
+printf "\n"
