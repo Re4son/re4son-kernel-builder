a7a92f95570ae2bdb0207e2c0b1236931cf3811f
Re4son <Re4son@users.noreply.github.com>
2018-10-07 22:05:58 +1100

    Clean up old firmware

--- install.sh
+++ install.sh
@@ -143,12 +143,13 @@ function install_kernel(){
     exitonerr dpkg --force-architecture -i libraspberrypi-dev_*
     exitonerr dpkg --force-architecture -i libraspberrypi-doc_*
     exitonerr dpkg --force-architecture -i libraspberrypi-bin_*
+    exitonerr dpkg --force-architecture -i raspberrypi-re4son-firmware_*
 
     ## Install nexmon firmware
     ARCH=`dpkg --print-architecture`
-    printf "\t**** Installing firmware                          ****\n"
-    cp -f firmware/brcmfmac* /lib/firmware/brcm/
-    printf "\t**** Firmware installed                           ****\n"
+    ##printf "\t**** Installing firmware                          ****\n"
+    ##cp -f firmware/brcmfmac* /lib/firmware/brcm/
+    ##printf "\t**** Firmware installed                           ****\n"
     printf "\n\t**** Installing nexutil ****\n"
     # Install nexutil
     exitonerr cp -f ./nexmon/${ARCH}/nexutil /usr/bin/
